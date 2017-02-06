18b4b78cbbe4f764c80494b2abb2096b79d6ea96
Jay <24zhangjie@gmail.com>
2017-02-06 21:09:00 +0800

    lambda test

---
+++ src/lambda/IState.java
@@ -0,0 +1,6 @@
+package lambda;
+
+public interface IState
+{
+	public void foo(String a);
+}
---
+++ src/lambda/TestLambda.java
@@ -0,0 +1,22 @@
+package lambda;
+
+public class TestLambda
+{
+	private IState s;
+
+	public void addState(IState s)
+	{
+		s.foo("jj");
+	}
+
+	public static void main(String[] ar)
+	{
+		TestLambda t = new TestLambda();
+
+		t.addState((String ss) -> System.out.println(ss));
+
+		new Thread(() -> System.out.println("the1")).start();
+		new Thread(() -> System.out.println("the2")).start();
+	}
+
+}
