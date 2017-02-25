b5b427104c29d0d64b999d67473e9a2d301851ab
Jay <24zhangjie@gmail.com>
2017-02-25 23:30:10 +0800

    update

--- src/collection/TestSet.java
+++ src/collection/TestSet.java
@@ -2,8 +2,10 @@ package collection;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * java.util.EnumSet<br>
@@ -29,7 +31,7 @@ public class TestSet
 		@Override
 		public int hashCode()
 		{
-			return 1;// super.hashCode();
+			return name.hashCode();// super.hashCode();
 		}
 
 		@Override
@@ -43,18 +45,27 @@ public class TestSet
 	public static void main(String[] ar)
 	{
 		/***
-		 * HashSet底层是用HashMap来实现的<>
+		 * HashSet底层是用HashMap来实现的<br>
+		 */
+
+		/**
+		 * 如果hashcode不同,但是依然有可能映射到HashSet中<br>
+		 * 如果hashcode相同,但是equals为false,那么它们依然是都可以存在于同一个HashSet中的<br>
+		 * 如果hashcode不同,但是equals为true,那么它们也依然是可以存在于同一个HashSet中的<br>
+		 * 如果hashcode相同,而且equals为true,那么就只有一个能够存在与HashSet中<br>
 		 */
 		Set<OS> hash = new HashSet<OS>();
 		hash.add(new OS("Jay"));
 		hash.add(new OS("Miao"));
 		hash.add(new OS("Miaommm"));
 		System.out.println(hash.size());
-
 		
+		LinkedHashSet<String> linked = new LinkedHashSet<>();
+		TreeSet<String> tree = new TreeSet<>();
+
 		/***
 		 * 
-		 * 如果两个key的hashcode不同,那么它们会在不同的位置上,因此不会冲突<br>
+		 * 如果两个key的hashcode不同,那么它们会在不同的位置上,因此不会冲突;但是依然有可能映射到同一个slot<br>
 		 * 如果两个key仅仅是hashcode相同的话,但是equals为false,那么它们都会存在于其中的一个链上<br>
 		 * 如果两个key的hashcode相同,而且equals为true,那么它们会被看做同一个key,只能存在其中一个<br>
 		 */
